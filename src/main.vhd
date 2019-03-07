9f680a670f5787a2490a847f18bf379a95472a18
jullinator <justus.karlsson@hotmail.se>
2019-03-08 00:07:25 +0100

    Added makefiles and scripts (in build), tried to begin at DataForwarding

---
+++ src/main.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity project is 
+    port (clk: std_logic;
+          rst: std_logic
+    );
+end project;
+
+architecture Behavioral of project is 
+
+
+
+
+begin
+    process (clk)
+    begin
+        if rising_edge(clk) then 
+            
+        end if;
+
+
+
+
+end Behavioral;
