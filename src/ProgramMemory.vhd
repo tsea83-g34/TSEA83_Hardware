77dce7b48c77f50787786fd19abf00dc91d1b9cc
angloth <angus.lothian@hotmail.com>
2019-03-26 12:01:22 +0100

    Add initial entity interface for program memory component

---
+++ src/ProgramMemory.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity program_memory_comp is 
+  port(
+    clk : in std_logic;
+    rst : in std_logic;
+
+    pm_control_signal : in unsigned(1 downto 0);
+
+    pm_write : in std_logic;
+    pm_write_data : in unsigned(31 downto 0);
+    pm_write_address : in unsigned(#PROGRAM_MEMORY_SIZE downto 0); -- TODO: update when program memory size is decided
+
+    pm_counter : buffer unsigned(#PROGRAM_MEMORY_SIZE downto 0); -- TODO: update when program memory size is decided
+    pm_out : out unsigned(31 downto 0)
+  );
+end program_memory_comp;
+
