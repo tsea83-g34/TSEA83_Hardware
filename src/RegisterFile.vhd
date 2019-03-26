017e4675813b2ce2441f7fc3fb2505a514257ccf
angloth <angus.lothian@hotmail.com>
2019-03-26 10:24:40 +0100

    Change register file addres to correct size (4 bit from 5 bit)

--- src/RegisterFile.vhd
+++ src/RegisterFile.vhd
@@ -3,14 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity register_file is
-  port(clk : in std_logic;
+  port (clk : in std_logic;
        rst : in std_logic;
 
-       addr_a : in unsigned(4 downto 0);
-       addr_b : in unsigned(4 downto 0);
+       addr_a : in unsigned(3 downto 0);
+       addr_b : in unsigned(3 downto 0);
 
        write_d : in std_logic; -- Should write
-       addr_d : in unsigned(4 downto 0);
+       addr_d : in unsigned(3 downto 0);
        data_d : in unsigned(31 downto 0);
 
        out_a : out unsigned(31 downto 0);
