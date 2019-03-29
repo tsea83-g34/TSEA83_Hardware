9844c9f4a6eda803c9672a83096a7e34e681b68f
jullinator <juska933@student.liu.se>
2019-03-29 13:41:08 +0100

    Pre Merge

--- src/DataForwarding/DataForwarding.vhd
+++ src/DataForwarding/DataForwarding.vhd
@@ -20,11 +20,7 @@ constant IR3_rom: unsigned(63 downto 0) = (others=>"0")
 constant IR4_rom: unsigned(63 downto 0) = (others=>"0") 
 
 
-alias IR2_op = unsigned(5 downto 0) is IR2(31 downto 26); 
-alias IR2_a = unsigned(4 downto 0) is IR2(20 downto 16); 
-alias IR2_b = unsigned(4 downto 0) is IR2(15 downto 11); 
-alias IR3_d = unsigned(4 downto 0) is IR3(25 downto 21);
-alias IR4_d = unsigned(4 downto 0) is IR4(25 downto 21);
+
 signal alu_a = unsigned(31 downto 0);
 
 begin
