ddb7b8512e96a6072197ce613040b1c1fa93aa89
angloth <angus.lothian@hotmail.com>
2019-03-29 14:37:42 +0100

    Add untested implementation of internal logic of RegisterFile, using register array to store registers

--- src/RegisterFile/RegisterFile.vhd
+++ src/RegisterFile/RegisterFile.vhd
@@ -20,4 +20,28 @@ entity register_file is
 end register_file;
 
 
-begin architecture Behavioral
+architecture Behavioral of register_file is
+  type reg_array is array (0 to 15) of unsigned(31 downto 0);
+  signal registers : reg_array := (others => X"00000000");
+begin
+  
+  -- Register file logic 
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' then
+        registers <= (others => X"00000000");
+      else 
+        -- 1. Update out_a and out_b registers based on addr_a and addr_b
+        out_a <= registers(to_integer(addr_a));
+        out_b <= registers(to_integer(addr_b));
+
+        -- 2. Check if should write, and if write data_d to addr_d.
+        if write_d = '1' then
+          registers(to_integer(addr_d)) <= data_d;
+        end if;
+      end if;
+    end if;
+  end process;
+
+end architecture;
