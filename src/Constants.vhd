032ffe39b0b81811270e61115235cbef4aacdae2
Henrik Nilsson <henni142@student.liu.se>
2019-04-02 16:35:26 +0200

    WIP: Added constants file for defining types and values used in multiple files.

---
+++ src/Constants.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+package PIPECPU_STD is
+  
+  type byte_mode is (WORD, HALF, BYTE)
+
+  constant DATA_MEM_BIT_SIZE = 8
+
+end PIPECPU_STD;
