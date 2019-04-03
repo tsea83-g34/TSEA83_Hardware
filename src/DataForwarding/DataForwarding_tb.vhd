0e9c1b4e6500bd236253afaa3d6578cc834fe97e
jullinator <juska933@student.liu.se>
2019-04-03 20:33:58 +0200

    Successfully tested 'assert' for modelsim with DataForwarding

--- src/DataForwarding/DataForwarding_tb.vhd
+++ src/DataForwarding/DataForwarding_tb.vhd
@@ -9,7 +9,7 @@ end DataForwarding_tb;
 
 architecture behavior of DataForwarding_tb is 
 
-  component data_forwarding
+  component DataForwarding
     port(
       clk : in std_logic;
       A2 : in unsigned(31 downto 0);
@@ -24,22 +24,6 @@ architecture behavior of DataForwarding_tb is
     );
   end component;
 
-  type test_record is record 
-    A2 : unsigned(31 downto 0);
-    B2 : unsigned(31 downto 0);
-    D3 : unsigned(31 downto 0);
-    D4 : unsigned(31 downto 0);
-    IMM1 : unsigned(31 downto 0);
-    control_signal : unsigned(5 downto 0);
-    ALU_a_out : unsigned(31 downto 0);
-    ALU_b_out : unsigned(31 downto 0);
-    AR_out : unsigned(31 downto 0);
-  end record; 
-  type test_record_array is array(natural range <>) of test_record;
-  constant test_records : test_record_array := (
-    -- A2, B2, D3, D4, IMM1, control_signal, ALU_a_out, ALU_b_out, AR_out
-    (X"1000_0000", X"2000_0000", X"0000_0000", X"0000_0000", X"0000_0000", "0_000_00", X"1000_0000", X"2000_0000", X"0000_0000")
-  );
 
   signal clk : std_logic;
   signal A2 : unsigned(31 downto 0);
@@ -58,7 +42,7 @@ architecture behavior of DataForwarding_tb is
 begin
 
   -- Component Instantiation
-  uut: data_forwarding port map(
+  uut: DataForwarding port map(
     clk => clk,
     A2 => A2,
     B2 => B2,
@@ -86,22 +70,22 @@ begin
 
   process
   begin
-    for i in test_records'range loop 
-      A2 <= test_records(i).A2
-      B2 <= test_records(i).B2
-      D3 <= test_records(i).D3
-      D4 <= test_records(i).D4
-      IMM1 <= test_records(i).IMM1
-      control_signal <= test_records(i).control_signal
-      
-      wait until rising_edge(clk);
-      assert (
-        (ALU_a_out = test_records(i).ALU_a_out) and (ALU_b_out = test_records(i).ALU_b_out) and (AR_out = test_records(i).AR_out)
-      )
-      report "Failed test " & integer'image(i) & "failed." severity error;
-    end loop;
-    -- Insert test here, and add more if you want 
     wait until rising_edge(clk);
+
+    A2 <= X"1000_0000";
+
+
+    assert (
+      ALU_a_out = X"0000_0000"
+    )
+    report "Failed {insert_name} . Expected output: 1"
+    severity error;
+    
+    wait until rising_edge(clk);
+
+    -- Insert additional tests here
+
+
     wait for 1 us;
     
     tb_running <= false;           
