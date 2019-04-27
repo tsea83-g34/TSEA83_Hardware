28dcf6941743b42992d0bc083b1f1a4de709ef4f
jullinator <juska933@student.liu.se>
2019-04-27 20:53:23 +0200

    Test does not compile, can not register the constants. Maybe bcz two main files..?

--- src/ControlUnit/ControlUnit.vhd
+++ src/ControlUnit/ControlUnit.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 library work;
-use work.PIPECPU.ALL; -- Include constants
+use work.PIPECPU_STD.ALL; -- Include constants
 
 entity control_unit is
   port (
