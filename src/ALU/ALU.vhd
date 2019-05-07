9c0086f87d80e8f4920a56baa3c5f6ff79bb497e
angloth <angus.lothian@hotmail.com>
2019-05-07 18:23:12 +0200

    Add new logic for shift instruction that depends on size.

--- src/ALU/ALU.vhd
+++ src/ALU/ALU.vhd
@@ -10,8 +10,8 @@ entity alu is
         rst : in std_logic;
         
         update_flags_control_signal : in unsigned(0 downto 0);
-        data_size_control_signal : in byte_mode;
-        alu_operation_control_signal : in unsigned(5 downto 0);
+        data_size : in byte_mode;
+        alu_op : in unsigned(5 downto 0);
 
         alu_a : in unsigned(31 downto 0); -- rA
         alu_b : in unsigned(31 downto 0); -- rB or IMM
@@ -29,7 +29,8 @@ architecture Behavioral of alu is
   
   signal alu_a_33 : unsigned(32 downto 0);
   signal alu_b_33 : unsigned(32 downto 0);
-  signal alu_res_33 : unsigned(32 downto 0);
+  signal alu_res_32 : unsigned(32 downto 0);
+  signal alu_shift_res_33 : unsigned(32 downto 0);
   signal alu_res_66 : unsigned(65 downto 0) := X"0000_0000_0000_0000" & "00";
 
   signal alu_res_n : unsigned(31 downto 0);
@@ -44,15 +45,52 @@ begin
   -- 2. Perform ALU operation and calculate result
   
   -- 2.A Perform 66 bit operation (multiply)
-  with alu_operation_control_signal select 
+  with alu_op select 
    alu_res_66 <= 
        unsigned(signed(alu_a_33) * signed(alu_b_33)) when MUL,
        X"0000_0000_0000_0000" & "00" when others;
   
-  -- TODO: 2.B Perform shifting ALU operation, that depends on size
+
+  -- 2.B Perform shifting ALU operation, which depends on size
+  process(alu_op, data_size, alu_a) 
+  begin
+    if alu_op = LSL then
+      case data_size is
+        when WORD =>
+          alu_shift_res_33 <= "0" & alu_a(30 downto 0) & "0";
+        when HALF =>
+          if alu_a(14 downto 14) = "1" then -- new MSB = 1
+            alu_shift_res_33 <= "0" & X"FFFF" & alu_a(14 downto 0) & "0"; -- Sign extend with "FFFF"
+          else                              -- new MSB = 0
+            alu_shift_res_33 <= "0" & X"0000" & alu_a(14 downto 0) & "0"; -- Sign extend with "0000"
+          end if;
+        when BYTE =>
+          if alu_a(6 downto 6) = "1" then -- new MSB = 1
+            alu_shift_res_33 <= "0" & X"FFFF_FF" & alu_a(6 downto 0) & "0"; -- Sign extend with "FFFF_FF"
+          else                            -- new MSB = 0
+            alu_shift_res_33 <= "0" & X"0000_00" & alu_a(6 downto 0) & "0"; -- Sign extend with "0000_00"
+          end if;
+        when others => 
+          alu_shift_res_33 <= ZERO; -- Undefined  
+      end case;      
+    else if alu_op = LSR then
+      case data_size is
+        when WORD =>
+          alu_shift_res_33 <= "0" & "0" & alu_a(31 downto 1); 
+        when HALF =>
+          alu_shift_res_33 <= "0" & X"0000" & alu_a(15 downto 1); -- Always sign extend with zeroes       
+        when BYTE =>
+          alu_shift_res_33 <= "0" & X"0000_00" & alu_a(7 downto 1); -- Always sign extend with zeroes 
+        when others =>
+          alu_shift_res_33 <= ZERO; -- Undefined
+      end case; 
+    else
+      alu_shift_res_33 <= ZERO;
+  end process;
+
 
   -- 2.C Perform regular ALU operation
-  with alu_operation_control_signal select
+  with alu_op select
     alu_res_33 <= 
                   alu_a_33 when ALU_PASS, -- Just pass:
                                           -- MOVE,  STORE, STORE_PM, STORE_VGA, OUT                 
@@ -65,8 +103,8 @@ begin
 
                   alu_res_66(32 downto 0) when MUL, -- MUL - multiplication for signed integers 
 
-                  alu_a_33(31 downto 0) & '0' when LSL, -- LSL
-                  '0' & alu_a_33(32 downto 1) when LSR, -- LSR
+                  alu_shift_res_33 when LSL, -- LSL
+                  alu_shift_res_33 when LSR, -- LSR
 
                   alu_a_33 and alu_b_33 when ANDD, -- AND 
                   alu_a_33 or alu_b_33 when ORR, -- OR 
@@ -87,11 +125,13 @@ begin
   -- 3. Calculate flags
   -- Zero flag
   Z_next <= '1' when alu_res_33(31 downto 0) = X"0000_0000" else '0';
+
   -- Negative flag
   N_next <= alu_res_33(31); -- Most significant bit of the result 
+
   -- Overflow flag
   -- Logic depends on operation
-  with alu_operation_control_signal select
+  with alu_op select
       O_next <= (alu_res_33(31) and not alu_a_33(31) and not alu_b_33(31)) or
                 (not alu_res_33(31) and alu_a_33(31) and alu_b_33(31)) 
               when ADD, -- ADD
@@ -106,6 +146,7 @@ begin
               when DEC, -- DEC
                 '0'
               when others;
+
   -- Carry flag
   C_next <= alu_res_33(32); -- Carry bit of of the result
 
