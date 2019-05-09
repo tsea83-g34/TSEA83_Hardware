5cabe05b24ab4334e23ac999306b843ac1227a00
angloth <angus.lothian@hotmail.com>
2019-05-09 15:35:46 +0200

    Rename alu_res_3 out to write_back_out_3 and change alu_res_3 to internal signal

--- src/WriteBackLogic/WriteBackLogic.vhd
+++ src/WriteBackLogic/WriteBackLogic.vhd
@@ -13,7 +13,7 @@ entity WriteBackLogic is
 
         write_back_control_signal : in unsigned(1 downto 0);
 
-        alu_res_3 : buffer unsigned(31 downto 0);
+        write_back_out_3 : buffer unsigned(31 downto 0);
         write_back_out_4 : out unsigned(31 downto 0)
   );
 end WriteBackLogic;
@@ -21,18 +21,22 @@ end WriteBackLogic;
 
 architecture Behavioral of WriteBackLogic is
   constant NOP : unsigned(31 downto 0) := (others => '0');
-
+  
+  signal alu_res_3 : unsigned(31 downto 0);
   signal alu_res_4 : unsigned(31 downto 0);
 
   alias in_or_alu3 : unsigned(0 downto 0) is write_back_control_signal(0 downto 0);
   alias dm_or_alu4 : unsigned(0 downto 0) is write_back_control_signal(1 downto 1);
 begin
   
-  -- Selection of alu_res_3 output, used for data_forwarding and in the second mux
+  -- Selection of alu_res_3, used in the second mux
   with in_or_alu3 select
     alu_res_3 <= alu_res when "0",
                  keyboard_out when "1",
                  NOP when others;
+
+  -- Output alu_res_3 for dataforwarding
+  write_back_out_3 <= alu_res_3;
   
   -- Update alu_res_4 value on clk cycle
   process(clk)
@@ -46,7 +50,7 @@ begin
     end if;
   end process;
 
-  -- Selection of wrrite_back_out_4 output, used for register write back and dataforwarding
+  -- Selection of write_back_out_4 output, used for register write back and dataforwarding
   with dm_or_alu4 select
     write_back_out_4 <= alu_res_4 when "0",
                         dm_out when "1",
