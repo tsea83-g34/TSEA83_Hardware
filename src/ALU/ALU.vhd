043ad6c37fc5adea6d300fc969a2e62af304e6c2
angloth <angus.lothian@hotmail.com>
2019-05-09 12:56:31 +0200

    Remove incorrect include from ALU.vhd

--- src/ALU/ALU.vhd
+++ src/ALU/ALU.vhd
@@ -1,11 +1,9 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-library work;
-use work.PIPECPU_STD.ALL;
 
 library work;
-use work.PIPECPU.ALL;
+use work.PIPECPU_STD.ALL;
 
 entity alu is
   port (
