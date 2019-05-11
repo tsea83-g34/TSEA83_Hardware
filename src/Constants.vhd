585065a53e2efcc576a73395d5f3c2ebc0e2f342
angloth <angus.lothian@hotmail.com>
2019-05-11 17:33:58 +0200

    Change std_logic control signals to enums in data forwarding, progress to #57

--- src/Constants.vhd
+++ src/Constants.vhd
@@ -43,6 +43,10 @@ package PIPECPU_STD is
 
   -- DataForwarding control signals
   type df_select is (DF_FROM_RF, DF_FROM_D3, DF_FROM_D4);
+
+  type df_alu_imm_or_b_enum is (DF_ALU_IMM, DF_ALU_B);
+  
+  type df_ar_a_or_b_enum is (DF_AR_A, DF_AR_B);
  
   -- Data memory control signal
   type dm_write_or_read_enum is (DM_WRITE, DM_READ);
