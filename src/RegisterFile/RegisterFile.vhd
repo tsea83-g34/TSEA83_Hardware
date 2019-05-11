2c0364169f30999b97c85e2424f32a9f775c0047
angloth <angus.lothian@hotmail.com>
2019-05-11 15:12:16 +0200

    Change register file control signals to enums in Register file component and test bench

--- src/RegisterFile/RegisterFile.vhd
+++ src/RegisterFile/RegisterFile.vhd
@@ -2,6 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+library work;
+use work.PIPECPU_STD.ALL;
+
 entity register_file is
   port (
         clk : in std_logic;
@@ -11,9 +14,10 @@ entity register_file is
         read_addr_b : in unsigned(3 downto 0);
 				read_addr_d : in unsigned(3 downto 0);
 				
-				read_d_or_b_control_signal : in std_logic; -- 1 => read addr_d, 0 => read addr_b
+				read_d_or_b_control_signal : in rf_read_d_or_b_enum; -- 1 => read addr_d, 0 => read addr_b
 
-        write_d_control_signal : in std_logic; -- Should write
+        write_d_control_signal : in rf_write_d_enum; -- Should write
+
         write_addr_d : in unsigned(3 downto 0);
         write_data_d : in unsigned(31 downto 0);
 
@@ -39,13 +43,15 @@ begin
         out_a <= registers(to_integer(read_addr_a));
 				-- Select between addr_b or addr_d
 				case read_d_or_b_control_signal is
-					when '1' => 
+					when RF_READ_D => 
 						out_b <= registers(to_integer(read_addr_d));
-					when others =>
+					when RF_READ_B =>
 						out_b <= registers(to_integer(read_addr_b));
 				end case; 
+
+
         -- 2. Check if should write, and if write write_data_d to write_addr_d.
-        if write_d_control_signal = '1' then
+        if write_d_control_signal = RF_WRITE_D then
           registers(to_integer(write_addr_d)) <= write_data_d;
         end if;
       end if;
