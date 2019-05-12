0147d1882c4c0a2bdd0cf86a17e7c169ff776805
angloth <angus.lothian@hotmail.com>
2019-05-12 13:17:24 +0200

    Add additional out_A1 to register file and it's test bench that fits with the new hardware relevant for #63

--- src/RegisterFile/RegisterFile.vhd
+++ src/RegisterFile/RegisterFile.vhd
@@ -21,8 +21,9 @@ entity RegisterFile is
         write_addr_d : in unsigned(3 downto 0);
         write_data_d : in unsigned(31 downto 0);
 
-        out_a : out unsigned(31 downto 0);
-        out_b : out unsigned(31 downto 0)
+        out_A1 : out unsigned(31 downto 0); -- Used for rjmpreg
+        out_A2 : out unsigned(31 downto 0);
+        out_B2 : out unsigned(31 downto 0)
   );
 end RegisterFile;
 
@@ -30,7 +31,22 @@ end RegisterFile;
 architecture Behavioral of RegisterFile is
   type reg_array is array (0 to 15) of unsigned(31 downto 0);
   signal registers : reg_array := (others => X"00000000");
+
+  signal out_A2_n : unsigned(31 downto 0);
+  signal out_B2_n : unsigned(31 downto 0);
+
 begin
+
+  -- Next A2 val
+  out_A2_n <= registers(to_integer(read_addr_a));
+  
+  -- Output A1
+  out_A1 <= out_A2_n;    
+
+  -- Next B2 val
+  with read_d_or_b_control_signal select
+  out_B2_n <= registers(to_integer(read_addr_d)) when RF_READ_D,
+              registers(to_integer(read_addr_b)) when RF_READ_B;  
 
   -- Register file logic
   process(clk)
@@ -39,16 +55,10 @@ begin
       if rst = '1' then
         registers <= (others => X"00000000");
       else
-        -- 1. Update out_a and out_b registers based on addr_a and addr_b/addr_d
-        out_a <= registers(to_integer(read_addr_a));
-				-- Select between addr_b or addr_d
-				case read_d_or_b_control_signal is
-					when RF_READ_D => 
-						out_b <= registers(to_integer(read_addr_d));
-					when RF_READ_B =>
-						out_b <= registers(to_integer(read_addr_b));
-				end case; 
-
+        -- 1. Update out_A2 and out_B2 registers
+        out_A2 <= out_A2_n;
+			  
+        out_B2 <= out_B2_n;
 
         -- 2. Check if should write, and if write write_data_d to write_addr_d.
         if write_d_control_signal = RF_WRITE_D then
