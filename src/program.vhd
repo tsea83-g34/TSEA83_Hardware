577fdfaa66a70173ab9da1868146a3a1d3270b7d
jullinator <juska933@student.liu.se>
2019-05-14 16:00:53 +0200

    Added good test program for load data forwarding

--- src/program.vhd
+++ src/program.vhd
@@ -9,18 +9,37 @@ package program_file is
   
   constant program : program_memory_array := (
 --$PROGRAM
-X"f30e0004", -- movhi r0 r14 4
-X"f70000b0", -- movlo r0 r0 176
-X"f72e0010", -- movlo r2 r14 16
-X"f3220029", -- movhi r2 r2 41
-X"eb120000", -- vgaw r1 r2 0
-X"f322002a", -- movhi r2 r2 42
-X"eb120001", -- vgaw r1 r2 1
-X"f322ffff", -- movhi r2 r2 -1
-X"f722ffff", -- movlo r2 r2 -1
-X"eb020000", -- vgaw r0 r2 0
-X"f32e0000", -- movhi r2 r14 0
-X"eb020001", -- vgaw r0 r2 1
+  X"8b110001", -- addi r1 r1 1
+  X"8b220002", -- addi r2 r2 2
+  X"8b330003", -- addi r3 r3 3
+  X"d3330000", -- store r3 r3 0 [4]
+  X"83530000", -- load r5 r3 0 [4]
+  X"8b550001", -- addi r5 r5 1 [ADD_r5]
+  X"e3050005", -- cmpi r5 5
+  X"1f00fffe", -- brne ADD_r5
+  X"8baafedc", -- addi r10 r10 0xfedc
+  X"d00a000a", -- store r0 r10 10 [1]
+  X"80a0000a", -- load r10 r0 10 [1]
+  X"f7dd000f", -- movlo r13 r13 15
+  X"8bffffff", -- addi r15 r15 -1
+  X"d3fd0000", -- store r15 r13 0
+  X"3300000d", -- rjmp SUBROUTINE
+  X"8b110001", -- addi r1 r1 1
+  X"8b220002", -- addi r2 r2 2
+  X"8b330003", -- addi r3 r3 3
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"03000000", -- nop
+  X"83df0000", -- load r13 r15 0 [SUBROUTINE]
+  X"8bff0001", -- addi r15 r15 1
+  X"8fdd001e", -- subi r13 r13 30
+  X"ef0d0000", -- rjmprg r13
 --$PROGRAM_END
 others => X"00000000"
 );
