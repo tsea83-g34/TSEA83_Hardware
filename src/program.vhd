989dfec9b5d692a2b21ed0bc0d47a4e07aa5f941
angloth <angus.lothian@hotmail.com>
2019-05-14 14:58:51 +0200

    Add meme program

--- src/program.vhd
+++ src/program.vhd
@@ -8,21 +8,20 @@ use work.PIPECPU_STD.ALL;
 package program_file is
   
   constant program : program_memory_array := (
---$PROGRAM
-X"f30e0004", -- movhi r0 r14 4
-X"f70000b0", -- movlo r0 r0 176
-X"f72e0010", -- movlo r2 r14 16
-X"f3220029", -- movhi r2 r2 41
-X"eb120000", -- vgaw r1 r2 0
-X"f322002a", -- movhi r2 r2 42
-X"eb120001", -- vgaw r1 r2 1
-X"f322ffff", -- movhi r2 r2 -1
-X"f722ffff", -- movlo r2 r2 -1
-X"eb020000", -- vgaw r0 r2 0
-X"f32e0000", -- movhi r2 r14 0
-X"eb020001", -- vgaw r0 r2 1
---$PROGRAM_END
-others => X"00000000"
+    --$PROGRAM
+    X"8b11000a", -- addi r1 r1 10
+    X"33000001", -- rjmp L1
+    X"93111000", -- add r1 r1 r1
+    X"cf210000", -- move r2 r1
+    X"97111000", -- sub r1 r1 r1
+    X"a3110000", -- dec r1 r1
+    X"ef010000", -- rjmprg r1
+    X"83010000", -- load r0 r1 0 [4]
+    X"83010001", -- load r0 r1 1 [4]
+    X"83010002", -- load r0 r1 2 [4]
+    X"83010003", -- load r0 r1 3 [4]
+    --$PROGRAM_END
+    others => X"00000000"
 );
 
 end program_file;
