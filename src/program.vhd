0c27074a58fcbd6c151927b4b60044c65ad456dd
angloth <angus.lothian@hotmail.com>
2019-05-15 15:28:29 +0200

    Add working reset via B8 button and test program for testing reset

--- src/program.vhd
+++ src/program.vhd
@@ -10,45 +10,31 @@ package program_file is
   constant program : program_memory_array := (
 --$PROGRAM
 X"8b111111", -- addi r1 r1 4369
-X"8b222222", -- addi r2 r2 8738
-X"8b333333", -- addi r3 r3 13107
-X"8b444444", -- addi r4 r4 17476
-X"8b555555", -- addi r5 r5 21845
-X"8b666666", -- addi r6 r6 26214
-X"8b777777", -- addi r7 r7 30583
-X"8b888888", -- addi r8 r8 34952
-X"8b999999", -- addi r9 r9 39321
-X"8baaaaaa", -- addi r10 r10 43690
-X"8bbbbbbb", -- addi r11 r11 48059
-X"8bcccccc", -- addi r12 r12 52428
-X"8bdddddd", -- addi r13 r13 56797
 X"8beeeeee", -- addi r14 r14 61166
 X"03000000", -- nop
 X"03000000", -- nop
 X"03000000", -- nop
 X"03000000", -- nop
-X"e7010000", -- out 0 r1
-X"e7020000", -- out 0 r2
-X"e7030000", -- out 0 r3
-X"e7040000", -- out 0 r4
-X"e7050000", -- out 0 r5
-X"e7060000", -- out 0 r6
-X"e7070000", -- out 0 r7
-X"e7080000", -- out 0 r8
-X"e7090000", -- out 0 r9
-X"e70a0000", -- out 0 r10
+X"e7010000", -- out 0 r1 [L0]
 X"03000000", -- nop
 X"03000000", -- nop
-X"33000000", -- rjmp 0
-X"e70b0000", -- out 0 r11
-X"e70c0000", -- out 0 r12
-X"e70d0000", -- out 0 r13
+X"f722f080", -- movlo r2 r2 61568
+X"f32202fa", -- movhi r2 r2 762
+X"a3220000", -- dec r2 r2 [L1]
+X"df002000", -- cmp r0 r2
+X"1f00fffe", -- brne L1
+X"03000000", -- nop
+X"03000000", -- nop
+X"03000000", -- nop
+X"03000000", -- nop
 X"e70e0000", -- out 0 r14
 X"03000000", -- nop
 X"03000000", -- nop
 X"03000000", -- nop
 X"03000000", -- nop
-
+X"33000000", -- rjmp L2 [L2]
+X"03000000", -- nop
+X"03000000", -- nop
 --$PROGRAM_END
 others => X"00000000"
 );
